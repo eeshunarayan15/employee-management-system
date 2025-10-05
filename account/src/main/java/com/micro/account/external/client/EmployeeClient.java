f113f9d255fd5113d4221fc30b759b7e6c32fcac
Eeshu Narayan <eeshunarayan15@gmail.com>
2025-10-05 14:52:49 +0530

    implemented eureka client

--- account/src/main/java/com/micro/account/external/client/EmployeeClient.java
+++ account/src/main/java/com/micro/account/external/client/EmployeeClient.java
@@ -6,13 +6,16 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+//
+//@FeignClient(
+//        name = "employee",
+//        url = "http://localhost:8081/api/employees/"
+//)
 @FeignClient(
-        name = "employee",
-        url = "http://localhost:8081/api/employees/"
+        name = "EMPLOYEE"
 )
 public interface EmployeeClient {
     //infrastructure configuration
-    @GetMapping("/{id}")
+    @GetMapping("api/employees/{id}")
     public ApiResponse<EmployeeResponse> getSingleEmployee(@PathVariable String id) ;
 }
